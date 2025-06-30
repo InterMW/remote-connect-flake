9b8e995f429aa8b5ea1e1ebbf9a974501f6bd21f
Joseph Melberg <josephbmelbergthird@outlook.com>
2025-06-30 21:52:55 +0000

    a little tedius

--- getport.sh
+++ getport.sh
@@ -1,4 +1,4 @@
 #!SHELL
-mac=$(getmac) | tr -d '\n'
+mac=getmac | tr -d '\n'
 echo $mac
 echo $(curl -d '{"serialNumber":"'$mac'", "ipAddress":"'$(curl https://ipinfo.io/ip)'"}' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register)
