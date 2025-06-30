cfd20a980c7c021fc5b7eea5a83bd55643340a08
Joseph Melberg <josephbmelbergthird@outlook.com>
2025-06-30 21:41:34 +0000

    more changes

--- script.sh
+++ script.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 echo $(getmac)
-echo '{"serialNumber":"'$(getmac)'", "ipAddress":"'$(curl -s https://ipinfo.io/ip)'"}"' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register
 e=$(curl -d '{"serialNumber":"'$(getmac)'", "ipAddress":"'$(curl https://ipinfo.io/ip)'"}' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register)
-echo $e
-echo $e
-echo $e
-echo $2
-echo $2
-echo $2
 echo $e
 autossh -M 32400 jump@jump.centurionx.net -R $e:localhost:22  -o "StrictHostKeyChecking no" 
 
