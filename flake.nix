222e540e184789a19c0071d726416b710a5ed85a
Joseph Melberg <josephbmelbergthird@outlook.com>
2025-07-01 16:14:32 +0000

    updating

--- flake.nix
+++ flake.nix
@@ -8,23 +8,53 @@
 
   outputs = { self, nixpkgs , getmac}:
     let
-      system = "aarch64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
     in
     {
-      packages.${system}= 
-	{
-		default = pkgs.writeShellApplication {
-		name = "remoteconnect";
-		runtimeInputs = [ pkgs.autossh ];
-		text = ''
-		  #!${pkgs.stdenv.shell}
-		  ${builtins.readFile ./script.sh}
-		'';
-		checkPhase = "${pkgs.stdenv.shellDryRun} $target";
-		};
+      # A Nixpkgs overlay.
+      overlay = final: prev: {
+
+        remoteconnect = with final; stdenv.mkDerivation rec {
+          name = "remoteconnect-${version}";
+
+          unpackPhase = ":";
+
+          buildPhase =
+            ''
+              cat > remoteconnect <<EOF
+              #! $SHELL
+	      ${builtins.readFile ./script.sh}
+              EOF
+              chmod +x getmac
+            '';
+
+          installPhase =
+            ''
+              mkdir -p $out/bin
+              cp remoteconnect $out/bin/
+            '';
+        };
 
       };
+      defaultPackage = forAllSystems (system: self.packages.${system}.getmac);
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) remoteconnect;
+        });
     };
+      nixosModules.remoteconnect =
+        { pkgs, ... }:
+        {
+          nixpkgs.overlays = [ self.overlay ];
+
+          environment.systemPackages = [ pkgs.remoteconnect ];
+
+          #systemd.services = { ... };
+        };
 }
 
