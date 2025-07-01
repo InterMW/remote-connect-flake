293f95a4154a124236b77f86cff7c3dd764cfe43
Joseph Melberg <josephbmelbergthird@outlook.com>
2025-07-01 21:37:46 +0000

    might just work

--- flake.nix
+++ flake.nix
@@ -6,55 +6,49 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.getmac.url = "github:InterMW/mac-flake";
 
-  outputs = { self, nixpkgs , getmac}:
+  outputs = { self, nixpkgs,  getmac}:
     let
       lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
       version = builtins.substring 0 8 lastModifiedDate;
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+	thesrc = builtins.readFile ./script.sh;
+
+
     in
     {
-      # A Nixpkgs overlay.
-      overlay = final: prev: {
-
-        remoteconnect = with final; stdenv.mkDerivation rec {
-          name = "remoteconnect-${version}";
-
-          unpackPhase = ":";
-
-          buildPhase =
-            ''
-              cat > remoteconnect <<EOF
-              #! $SHELL
-	      ${builtins.readFile ./script.sh}
-              EOF
-              chmod +x getmac
-            '';
-
-          installPhase =
-            ''
-              mkdir -p $out/bin
-              cp remoteconnect $out/bin/
-            '';
-        };
-
-      };
-      defaultPackage = forAllSystems (system: self.packages.${system}.getmac);
+	overlay = final: prev: {
+		remoteconnect = with final; stdenv.mkDerivation rec {
+		name = "remoteconnect-${version}";
+		unpackCmd = ''
+		mkdir test-src
+		cp $curSrc test-src/script.sh
+		'';
+		src = ./script.sh;
+		installPhase = ''
+		mkdir -p $out/bin
+		cp ${src} $out/bin/remoteconnect
+		'';
+		};
+	};
+	
+      defaultPackage = forAllSystems (system: self.packages.${system}.remoteconnect);
       # Provide some binary packages for selected system types.
       packages = forAllSystems (system:
         {
           inherit (nixpkgsFor.${system}) remoteconnect;
         });
-    };
       nixosModules.remoteconnect =
         { pkgs, ... }:
         {
           nixpkgs.overlays = [ self.overlay ];
 
-          environment.systemPackages = [ pkgs.remoteconnect ];
+          environment.systemPackages = [ self.packages.remoteconnect ];
 
           #systemd.services = { ... };
         };
+    };
+
 }
 
--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 autossh -M 32400 jump@jump.centurionx.net -R $(curl -s -d '{"serialNumber":"'$(getmac)'", "ipAddress":"'$(curl -s https://ipinfo.io/ip)'"}' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register):localhost:22  -o "StrictHostKeyChecking no" 
