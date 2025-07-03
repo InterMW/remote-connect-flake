f3bd15c67ba7f777c6e84e20892fb5fc4838305d
Joseph Melberg <josephbmelbergthird@outlook.com>
2025-07-03 21:08:43 +0000

    updating target

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-autossh -N -M 32400 jump@jump.centurionx.net -R $(curl -s -d '{"serialNumber":"'$(getmac)'", "ipAddress":"'$(curl -s https://ipinfo.io/ip)'"}' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register):localhost:22  -o "StrictHostKeyChecking no" 
+autossh -N -M 32400 jump@jumppad.centurionx.net -R $(curl -s -d '{"serialNumber":"'$(getmac)'", "ipAddress":"'$(curl -s https://ipinfo.io/ip)'"}' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register):localhost:22  -o "StrictHostKeyChecking no" 
