8cf8e67cd8bc433b9f91b9a7e148060a8bcc3ae6
Joseph-Melberg <josephbmelbergthird@outlook.com>
2025-11-03 11:22:57 -0600

    Update script.sh
    Why must I do this?

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh jump@jumppad.centurionx.net -R $(curl -s -d '{"serialNumber":"'$(getmac)'", "ipAddress":"'$(curl -s https://ipinfo.io/ip)'"}' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register):localhost:22  -o "StrictHostKeyChecking no" -N
+ssh jump@jumppad.centurionx.net -R $( curl -s -d '{"serialNumber":"'$(getmac)'", "ipAddress":"'$(curl -s https://ipinfo.io/ip)'"}' -H "Content-Type: application/json" -X POST https://api.centurionx.net/device/register):localhost:22  -o "StrictHostKeyChecking no" -N
